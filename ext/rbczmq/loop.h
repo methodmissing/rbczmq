a332017def69c8191fa58b006c8b84c78e920fc8
Lourens Naudé <lourens@methodmissing.com>
2011-12-01 02:49:08 +0000

    Assert ZMQ::Loop instances as well

--- ext/rbczmq/loop.h
+++ ext/rbczmq/loop.h
@@ -6,8 +6,10 @@ typedef struct {
     Bool running;
 } zmq_loop_wrapper;
 
+#define ZmqAssertLoop(obj) ZmqAssertType(obj, rb_cZmqLoop, "ZMQ::Loop")
 #define ZmqGetLoop(obj) \
     zmq_loop_wrapper *loop = NULL; \
+    ZmqAssertLoop(obj); \
     Data_Get_Struct(obj, zmq_loop_wrapper, loop); \
     if (!loop) rb_raise(rb_eTypeError, "uninitialized ZMQ loop!");
 
