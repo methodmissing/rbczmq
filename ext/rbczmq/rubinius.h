262b529480d982caf9039202be7aab5f02e84661
Lourens Naudé <lourens@methodmissing.com>
2012-01-26 02:44:31 +0000

    Further rbx 1.9 specifix fixes

--- ext/rbczmq/rubinius.h
+++ ext/rbczmq/rubinius.h
@@ -37,14 +37,16 @@
 #ifndef RBCZMQ_RUBINIUS_H
 #define RBCZMQ_RUBINIUS_H
 
-#include "st.h"
-
 #define RSTRING_NOT_MODIFIED
 
 #ifdef HAVE_RUBY_ENCODING_H
+#include <ruby/st.h>
+#include <ruby/encoding.h>
+#include <ruby/io.h>
 extern rb_encoding *binary_encoding;
 #define ZmqEncode(str) rb_enc_associate(str, binary_encoding)
 #else
+#include "st.h"
 #define ZmqEncode(str) str
 #endif
 
