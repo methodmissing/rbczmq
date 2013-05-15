95d4e71d2082f4c81d080e1589596e0dd50b61f2
Lourens Naudé <lourens@methodmissing.com>
2013-05-15 11:33:18 +0100

    Flag JRuby as unsupported

--- ext/rbczmq/jruby.h
+++ ext/rbczmq/jruby.h
@@ -1,22 +1,6 @@
 #ifndef RBCZMQ_JRUBY_H
 #define RBCZMQ_JRUBY_H
 
-#include "st.h"
-
-/* XXX */
-#define ZmqEncode(str) str
-#ifndef THREAD_PASS
-#define THREAD_PASS rb_thread_schedule();
-#endif
-
-#define TRAP_BEG
-#define TRAP_END
-
-#undef rb_errinfo
-#define rb_errinfo() (rb_gv_get("$!"))
-
-#define ZMQ_DEFAULT_SOCKET_TIMEOUT Qnil
-
-#define HAVE_RB_THREAD_BLOCKING_REGION 1
+#error Unsupported Ruby platform - this extension only works with Ruby 1.9, Ruby 2 and Rubinius
 
 #endif
