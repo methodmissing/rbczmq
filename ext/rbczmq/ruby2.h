8c4bb369c4f6eed2f65ff4e7c58324e3cfb3f9c4
Lourens Naudé <lourens@methodmissing.com>
2014-12-24 18:44:33 +0200

    Better redef of rb_thread_blocking_region

--- ext/rbczmq/ruby2.h
+++ ext/rbczmq/ruby2.h
@@ -1,8 +1,9 @@
 #ifndef RBCZMQ_RUBY2_H
 #define RBCZMQ_RUBY2_H
 
-#ifndef HAVE_RB_THREAD_CALL_WITHOUT_GVL
-#define rb_thread_call_without_gvl rb_thread_blocking_region
+#ifdef HAVE_RB_THREAD_BLOCKING_REGION
+#define rb_thread_call_without_gvl(func, data1, ubf, data2) \
+  rb_thread_blocking_region((rb_blocking_function_t *)func, data1, ubf, data2)
 #endif
 
 #endif
