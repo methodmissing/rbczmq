bf093fa4c3fd741dc70bfb4ac2696f7c7f089b94
Lourens Naudé <lourens@methodmissing.com>
2014-12-24 16:17:44 +0200

    Prefer GVL method check by constant

--- ext/rbczmq/ruby2.h
+++ ext/rbczmq/ruby2.h
@@ -1,7 +1,7 @@
 #ifndef RBCZMQ_RUBY2_H
 #define RBCZMQ_RUBY2_H
 
-#ifndef rb_thread_call_without_gvl
+#ifndef HAVE_RB_THREAD_CALL_WITHOUT_GVL
 #define rb_thread_call_without_gvl rb_thread_blocking_region
 #endif
 
